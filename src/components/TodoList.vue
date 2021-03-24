8bdf0bac44f4e0a5fc3b0bfd8107500f0a063ee9
wjdqkdwo@naver.com <jaejung603@gmail.com>
2021-03-25 00:05:45 +0900

    todoList(checkbox & deleteBtn) CSS

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,7 +2,14 @@
   <section>
     <ul>
       <li v-for="todoItem in todoItems" v-bind:key="todoItem">
-        {{ todoItem }}
+        <i class="checkBtn fas fa-check"></i>{{ todoItem }}
+        <span
+          class="removeBtn"
+          type="button"
+          @click="removeTodo(todoItem, index)"
+        >
+          <i class="fas fa-trash"></i
+        ></span>
       </li>
     </ul>
   </section>
@@ -23,4 +30,32 @@ export default {
   }
 };
 </script>
-<style></style>
+<style>
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+</style>
